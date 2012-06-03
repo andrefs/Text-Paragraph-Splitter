6ba247d40bd6a121718351d1776bd2f20de5b84f
André Santos <andrefs@cpan.org>
2012-06-03 23:36:00 +0100

    kickoff and skeletons for main methods

--- lib/Text/Paragraph/Splitter.pm
+++ lib/Text/Paragraph/Splitter.pm
@@ -1,7 +1,44 @@
-use strict;
-use warnings;
-package Text::Paragraph::Splitter;
+use strict; use warnings;
+use MooseX::Declare;
 
-# ABSTRACT: Doesn't matter what it does, it is awesome anyway! (TODO: change this -- ups!)
+# ABSTRACT: Guesses the notation used for paragraphs to split them.
+
+
+class Text::Paragraph::Splitter {
+	has 'blw'	=> ( is => 'rw', isa => Num,  default => 0.8 );  # blank line weigth
+	has 'sl' 	=> ( is => 'rw', isa => Int,  default => 50  );  # short length
+	has 'slw'	=> ( is => 'rw', isa => Num,  default => 0.5 );  # short line weigth
+	has 'indl'	=> ( is => 'rw', isa => Int,  default => 2   );  # Mininum length of white space needed to consider line indented
+	has 'indw'	=> ( is => 'rw', isa => Num,  default => 0.3 );  # indented line weigth
+	has 'ie'	=> ( is => 'rw', isa => Bool, default => 0   );  # include trailing white space in paragraphs?
+	has 'ptag'	=> ( is => 'rw', isa => Str,  default => 'P' );  # Tag to be used to delimit paragraphs.
+	has 'ptres' => ( is => 'rw', isa => Num,  default => 0.5 );  # Minimum confidence needed to consider paragraph
+
+	method offsets (Str|ScalarRef[Str] $text) {
+	}
+
+	method split (Str|ScalarRef[Str] $text) {
+	}
+
+	method annotate (Str|ScalarRef[Str] $text) {
+	}
+
+	
+}
 
 1;
+
+=method offsets
+
+Finds paragraphs boundaries and returns a reference to an array of pairs, each consisting of a paragraph's first and last character offsets.
+
+=method split
+
+Split the paragraphs and returns them in an array reference.
+
+=method annotate
+
+Returns the original text with paragraphs annotated XML-style.
+
+=cut
+
